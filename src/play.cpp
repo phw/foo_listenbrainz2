a201043c4559ee8971a29ca87729b5cd3ba7f1ac
Philipp Wolfer <ph.wolfer@gmail.com>
2022-09-03 14:07:46 +0200

    Only submit actual version number as media_player_version

--- src/play.cpp
+++ src/play.cpp
@@ -187,7 +187,7 @@ namespace lbz
 
 			if (prefs::check_client_details.get_value()) {
 				additional_info["media_player"] = "foobar2000";
-				additional_info["media_player_version"] = core_version_info::g_get_version_string();
+				additional_info["media_player_version"] = core_version_info_v2::get()->get_version_as_text();
 				additional_info["submission_client"] = "foobar2000";
 				additional_info["submission_client_version"] = component_version;
 			}
